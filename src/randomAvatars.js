3d154b279fedd0b00ff88422e2fa9ff7b2a41e9d
mikfairo <mikfairo123@gmail.com>
2025-11-16 23:48:43 -0500

    RandomAvatars is now making AJAX requests instead of using math.random

---
+++ src/randomAvatars.js
@@ -0,0 +1,11 @@
+async function getRandomUrl() {
+
+  const response = await fetch('http://localhost:3000/get_random_seed')
+  const data = await response.json()
+  const seed = data.seed[0]
+  const url = `https://api.dicebear.com/9.x/notionists/svg?seed=${seed}`;
+  console.log(url)
+  return url;
+}
+
+export { getRandomUrl };
