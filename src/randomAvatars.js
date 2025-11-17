1f35751732fa97f52c21e3716c366f38775dc5cd
mikfairo <mikfairo123@gmail.com>
2025-11-16 23:57:01 -0500

    Added try catch so if not running on local host the url will still return.

--- src/randomAvatars.js
+++ src/randomAvatars.js
@@ -1,11 +1,17 @@
 async function getRandomUrl() {
+  try {
 
-  const response = await fetch('http://localhost:3000/get_random_seed')
-  const data = await response.json()
-  const seed = data.seed[0]
-  const url = `https://api.dicebear.com/9.x/notionists/svg?seed=${seed}`;
-  console.log(url)
-  return url;
+    const response = await fetch('http://localhost:3000/get_random_seed')
+    const data = await response.json()
+    const seed = data.seed[0]
+    const url = `https://api.dicebear.com/9.x/notionists/svg?seed=${seed}`;
+    console.log(url)
+    return url;
+  } catch {
+    const seed = Math.random()
+    const url = `https://api.dicebear.com/9.x/notionists/svg?seed=${seed}`;
+    return url
+  }
 }
 
 export { getRandomUrl };
