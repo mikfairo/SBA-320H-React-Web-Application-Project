03fef3dc4609635ea6a75274a3e3ebcfb2bbad5d
mikfairo <mikfairo123@gmail.com>
2025-11-16 22:17:22 -0500

    Added messages context provider.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { MessagesContext } from "./assets/MessagesContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <MessagesContext.Provider value={[]}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </MessagesContext.Provider>
+);
